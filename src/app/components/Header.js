c5aad2fd42b1950ba750361c5e7fba6a906ffd46
vickymarz <okorojiebube2@gmail.com>
2022-10-10 16:21:48 +0100

    Add logo

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
-
 import Nav from './Nav'
+import logo from '../../assets/images/logo.png'
 
 const Header = () => {
   return (
     <>
     <header>
         <Nav />
+        <img src={logo} alt='logo' />
     </header>
 
     </>
