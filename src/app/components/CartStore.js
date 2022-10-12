269a5419380e5ee39bb8fd7e5e6f7858e1c4022f
vickymarz <okorojiebube2@gmail.com>
2022-10-12 14:56:03 +0100

    Create Cartstore component

---
+++ src/app/components/CartStore.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CartStore = () => {
+  return (
+    <div>
+        <span>
+           <h2>My Bag</h2>,
+           3 items
+        </span>
+    </div>
+  )
+}
+
+export default CartStore
